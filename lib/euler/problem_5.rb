0b3588ee9d9db6bf7db7d30e4c07512f778061cc
Marcus Ortiz <mportiz08@gmail.com>
2013-01-29 13:11:34 -0800

    Optimized problem 5 runtime.

--- lib/euler/problem_5.rb
+++ lib/euler/problem_5.rb
@@ -1,16 +1,46 @@
+require 'euler/utils/prime_generator'
+
 module Euler
   class Problem5
-    # TODO: make faster
+    include Utils
+    
+    def initialize
+      @primes = PrimeGenerator.new.generate_upto(21)
+    end
+    
     def solve
-      i = 20
-      i += 20 until evenly_divisible?(i, (1..20))
-      i
+      pfactors = (1..20).map { |i| pfactors_for(i) }
+      powers   = greatest_powers(pfactors)
+      
+      powers.map { |p| p.reduce(&:*) }.reduce(&:*)
     end
     
     private
     
-    def evenly_divisible?(num, divisors)
-      divisors.all? { |i| (num % i).zero? }
+    def greatest_powers(factors)
+      powers = []
+      @primes.each do |p|
+        sorted_by_count = factors.sort_by { |f| f.count(p) }
+        powers << sorted_by_count.last.select { |i| i == p }
+      end
+      powers
+    end
+    
+    def pfactors_for(num)
+      factors = []
+      if @primes.include?(num)
+        factors << num
+        return factors
+      end
+      @primes.each do |p|
+        div = num / p
+        if (num % p).zero?
+          factors << p
+          pfactors_for(div).each { |f| factors << f }
+          break
+        end
+      end
+      factors
     end
   end
 end
