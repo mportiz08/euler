58a44a0a940530e9b96e6eefb48135fdf46f394d
Marcus Ortiz <mportiz08@gmail.com>
2013-02-10 17:25:50 -0800

    Solved problem 22.

--- lib/euler/problem_22.rb
+++ lib/euler/problem_22.rb
@@ -6,7 +6,13 @@ module Euler
     
     def solve
       raw_data = DataFile.read('names.txt')
-      names    = raw_data.gsub('"', '').split(',')
+      names    = raw_data.gsub('"', '').split(',').sort
+      
+      scores = names.each_with_index.map do |name, i|
+        name.split('').map { |c| c.ord - 'A'.ord + 1 }.reduce(&:+) * (i + 1)
+      end
+      
+      scores.reduce(&:+)
     end
   end
 end
