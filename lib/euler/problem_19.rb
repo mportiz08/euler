08e4f9b109c25a69263978fc5f9988cd5a61d0fe
Marcus Ortiz <mportiz08@gmail.com>
2013-02-26 15:24:32 -0800

    Solved problem 19.

---
+++ lib/euler/problem_19.rb
@@ -0,0 +1,12 @@
+require 'date'
+
+module Euler
+  class Problem19
+    def solve
+      startd = Date.new(1901, 1, 1)
+      endd   = Date.new(2000, 12, 31)
+      
+      (startd..endd).select { |date| date.day == 1 && date.sunday? }.size
+    end
+  end
+end
