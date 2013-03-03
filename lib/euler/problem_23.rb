442d9c082fe97c384de5f2584a7ffd8c2f37cf45
Marcus Ortiz <mportiz08@gmail.com>
2013-03-02 16:45:28 -0800

    Much closer for problem 23.

--- lib/euler/problem_23.rb
+++ lib/euler/problem_23.rb
@@ -2,28 +2,26 @@ require 'core_ext/numeric'
 
 module Euler
   class Problem23
-    UPPER_LIMIT = 14062#28123
+    UPPER_LIMIT = 28123
     
     def initialize
-      all = []
-      (0..UPPER_LIMIT).each do |i|
-        all[i] = i.proper_divisors.sum > i
-      end
+      all, @non_abundant_sums = [], []
       
-      @nums = []
-      all.each_with_index do |_, n|
-        next unless n > 0 && n.even?
-        puts n
-        subnums = []
-        all[0...n].each_with_index { |abundant, i| subnums << i if abundant && i > 0 }
-        sums = subnums.product(subnums).map(&:sum)
+      (0..UPPER_LIMIT).each do |n|
+        all[n] = n.proper_divisors.sum > n
         
-        @nums << n unless sums.include?(n)
+        abundant_sum_exists = false
+        all[0...n].each_with_index do |abundant, i|
+          j = n - i
+          abundant_sum_exists = true if abundant && all[j]
+        end
+        
+        @non_abundant_sums << n unless abundant_sum_exists
       end
     end
     
     def solve
-      @nums.sum
+      @non_abundant_sums.sum
     end
   end
 end
